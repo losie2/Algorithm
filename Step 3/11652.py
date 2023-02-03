4a7a5918dea0aa3c8b4a3996e86040282e314cdf
kt-gram <dlsgh1347@naver.com>
2023-02-03 15:27:24 +0900

    Step 3 Update

---
+++ Step 3/11652.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = {}
+
+for i in range(N):
+    card = int(input())
+    if card in nums:
+        nums[card] += 1
+    else:
+        nums[card] = 1
+result = sorted(nums.items(), key = lambda x : (-x[1], x[0]))
+print(result[0][0])
+
+
+
